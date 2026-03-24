cf9a1f88c473ffb9ced087853cd6d070c9e233ec
Rodrigo Jr <rodrigojr.rezende@gmail.com>
2026-03-23 21:38:42 -0300

    Projeto criado

---
+++ src/main/java/com/devRodrigoJr/courseSpringBootJPA/CourseSpringBootJpaApplication.java
@@ -0,0 +1,12 @@
+package com.devRodrigoJr.courseSpringBootJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseSpringBootJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseSpringBootJpaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/devRodrigoJr/courseSpringBootJPA/CourseSpringBootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devRodrigoJr.courseSpringBootJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseSpringBootJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
